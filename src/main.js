778bb2056649f9c610bd02dc2df6b88f0407704b
Kevin Lu <kevin.lu@uxpsystems.com>
2017-11-20 12:18:56 -0500

    restructure file system

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
-import App from './App';
+import { App } from './app';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
-  render: h => h(App),
+  template: '<App/>',
+  components: { App },
+  render: h => h(App)
 });
